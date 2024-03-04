a53d42b4bbe245dc762f91f1c099f68a1bc51d91
Spencer Davis <9757975+strdavis@users.noreply.github.com>
2024-03-04 13:27:50 -0800

    Add TEST_VAR_3.

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.listen(port, () => {
 
   const testString1 = process.env.TEST_VAR_1
   const testString2 = process.env.TEST_VAR_2
+  const testString3 = process.env.TEST_VAR_3
   console.log(testString1)
   console.log(testString2)
+  console.log(testString3)
 })
 
 app.get('/', (req, res) => {
